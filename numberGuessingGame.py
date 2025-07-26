88bf56fdbafa498945f3381ab8a7564e4b64e1e3
Ayman Tahiat Faiyaz <tahiat0967@gmail.com>
2025-07-26 19:01:54 +0600

    Made a 'Number Guessing Game' project

---
+++ numberGuessingGame.py
@@ -0,0 +1,60 @@
+# Number Guessing Game
+
+# Computer randomly chooses a number
+
+# You guess, it tells you “Too High / Too Low”
+
+# Loop until correct
+import random
+
+tries = 0
+
+guess = 0
+
+while True:
+    difficulty = input(
+        "What level do you want? (easy, medium, hard, extreme, impossible): "
+    )
+
+    if difficulty.lower() == "easy":
+        start = 1
+        end = 10
+        number = random.randint(start, end)
+        break
+    elif difficulty.lower() == "medium":
+        start = 1
+        end = 25
+        number = random.randint(start, end)
+        break
+    elif difficulty.lower() == "hard":
+        start = 1
+        end = 50
+        number = random.randint(start, end)
+        break
+    elif difficulty.lower() == "extreme":
+        start = 1
+        end = 100
+        number = random.randint(start, end)
+        break
+    elif difficulty.lower() == "impossible":
+        start = 1
+        end = 1000
+        number = random.randint(start, end)
+        break
+    else:
+        print("Select an existing level!")
+
+while guess != number:
+    guess = int(input(f"Guess a number between {start} and {end}: "))
+    tries += 1
+
+    if guess > end or guess < start:
+        print("You cannot guess a number out of range!")
+    elif guess > number:
+        print("Oh no! It's too high!")
+    elif guess < number:
+        print("Oh no! It's too low!")
+    elif guess == number:
+        print(
+            f"You guessed it in just {tries} tries! That was {'insane' if tries <= 3 else 'pretty good'} 😎")
+        break
